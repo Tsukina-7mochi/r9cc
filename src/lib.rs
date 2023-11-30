7e3fa8b93a28c6cfaa687e7aa04757651cfe2a22
Tsukina-7mochi <tsukina7mochi@gmail.com>
2023-11-30 18:45:57 +0900

    refactor: replace huge string literal into String::push_str

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,12 @@
 pub fn compile(text: String) -> String {
     let text = text.trim();
+    let mut result = String::new();
 
-    format!(
-        "\
-.intel_syntax noprefix
-.global main
-main:
-    mov rax, {}
-    ret",
-        text.parse::<i32>().unwrap()
-    )
+    result.push_str(".intel_syntax noprefix\n");
+    result.push_str(".global main\n");
+    result.push_str("main:\n");
+    result.push_str(&format!("   mov rax, {}\n", text.parse::<i32>().unwrap()));
+    result.push_str("ret\n");
+
+    result
 }
