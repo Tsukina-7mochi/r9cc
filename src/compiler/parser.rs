7aa48aec8f3cdd620b3deeccc1b58a48760d56ba
Tsukina-7mochi <tsukina7mochi@gmail.com>
2024-04-05 16:32:54 +0900

    fix: fix problem on building error at the end or input

--- src/compiler/parser.rs
+++ src/compiler/parser.rs
@@ -32,7 +32,12 @@ impl<'a> Parser<'a> {
     }
 
     fn error_unexpected_token(&mut self, expected: Vec<TokenKind>) -> CompileError {
-        CompileError::unexpected_token(self.text, self.current_index_in_text().unwrap(), expected)
+        CompileError::unexpected_token(
+            self.text,
+            self.current_index_in_text()
+                .unwrap_or_else(|| self.text.len()),
+            expected,
+        )
     }
 
     fn consume_expr(&mut self) -> Result<Node> {
